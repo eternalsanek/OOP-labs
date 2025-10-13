46952f6941cdde5255cf155ec85873108e0e43ce
Maslov <sanek@google.com>
2025-10-13 13:08:27 +0400

    Добавлен пакет exceptions

---
+++ Lab2/src/main/java/exceptions/ArrayIsNotSortedException.java
@@ -0,0 +1,12 @@
+package exceptions;
+
+public class ArrayIsNotSortedException extends RuntimeException {
+    public ArrayIsNotSortedException() {
+        super();
+    }
+
+    public ArrayIsNotSortedException(String message) {
+        super(message);
+    }
+
+}
