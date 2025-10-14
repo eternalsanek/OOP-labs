20d8e86d373d4395e9423138453cfd266d01b7ce
Maslov <sanek@google.com>
2025-10-14 10:06:11 +0400

    Изменение ArrayTabulatedFunction: переписан метод iterator

--- Lab2/src/main/java/functions/ArrayTabulatedFunction.java
+++ Lab2/src/main/java/functions/ArrayTabulatedFunction.java
@@ -4,6 +4,7 @@ import exceptions.InterpolationException;
 
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class ArrayTabulatedFunction extends AbstractTabulatedFunction implements Insertable, Removable {
 
@@ -168,7 +169,26 @@ public class ArrayTabulatedFunction extends AbstractTabulatedFunction implements
     }
 
     @Override
-    public Iterator<Point> iterator(){
-        throw new UnsupportedOperationException();
+    public Iterator<Point> iterator() {
+        return new Iterator<Point>() {
+            private int i = 0;
+
+            @Override
+            public boolean hasNext() {
+                return i < count;
+            }
+
+            @Override
+            public Point next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+
+                Point point = new Point(xValues[i], yValues[i]);
+                i++;
+                return point;
+            }
+        };
     }
+
 }
