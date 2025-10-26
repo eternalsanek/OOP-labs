c6117015c9cb51a0be33ae9d0010f36de20a3456
Maslov <sanek@google.com>
2025-10-26 16:11:29 +0400

    Изменение TabulatedFunctionOperationService: 1) добавлено поле фабрики TabulatedFunctionFactory factory; 2) добавлены конструкторы; 3) добавлены геттер и сеттер для фабрики; 4) добавлен приватный интерфейс BiOperation с единственным методом: double apply(double u, double v); 5) создан и реализован приватный метод TabulatedFunction doOperation(TabulatedFunction a, TabulatedFunction b, BiOperation operation); 6) добавлены публичные методы сложения sum, subtract, которые делегируют своё поведение методу doOperation().

--- Lab2/src/main/java/operations/TabulatedFunctionOperationService.java
+++ Lab2/src/main/java/operations/TabulatedFunctionOperationService.java
@@ -1,13 +1,39 @@
 package operations;
 
+import exceptions.InconsistentFunctionsException;
 import functions.Point;
 import functions.TabulatedFunction;
+import functions.factory.ArrayTabulatedFunctionFactory;
+import functions.factory.TabulatedFunctionFactory;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 
 public class TabulatedFunctionOperationService {
+
+    TabulatedFunctionFactory factory;
+
+    private interface BiOperation {
+        double apply(double u, double v);
+    }
+
+    public TabulatedFunctionOperationService(TabulatedFunctionFactory factory) {
+        this.factory = factory;
+    }
+
+    public TabulatedFunctionOperationService() {
+        this.factory = new ArrayTabulatedFunctionFactory();
+    }
+
+    public TabulatedFunctionFactory getFactory() {
+        return factory;
+    }
+
+    public void setFactory(TabulatedFunctionFactory factory) {
+        this.factory = factory;
+    }
+
     public static Point[] asPoints(TabulatedFunction tabulatedFunction) {
         Point[] points = new Point[tabulatedFunction.getCount()];
 
@@ -17,5 +43,37 @@ public class TabulatedFunctionOperationService {
         }
 
         return points;
+    }
+
+    private TabulatedFunction doOperation(TabulatedFunction a, TabulatedFunction b, BiOperation operation) {
+        if (a.getCount() != b.getCount()) throw new InconsistentFunctionsException();
+
+        Point[] pointsF = asPoints(a);
+        Point[] pointsG = asPoints(b);
+
+        double[] xValues = new double[a.getCount()];
+        double[] yValues = new double[a.getCount()];
+
+        for (int i = 0; i < a.getCount(); ++i) {
+            if (Math.abs(pointsF[i].x - pointsG[i].x) > 1e-9) throw new InconsistentFunctionsException();
+            xValues[i] = pointsF[i].x;
+            yValues[i] = operation.apply(pointsF[i].y, pointsG[i].y);
+        }
+
+        return factory.create(xValues, yValues);
+    }
+
+    public TabulatedFunction sum(TabulatedFunction F, TabulatedFunction G) {
+        return doOperation(F, G, new BiOperation() {
+            @Override
+            public double apply(double u, double v) { return u + v; }
+        });
+    }
+
+    public TabulatedFunction subtract(TabulatedFunction F, TabulatedFunction G) {
+        return doOperation(F, G, new BiOperation() {
+            @Override
+            public double apply (double u, double v) { return u - v; }
+        });
     }
 }
