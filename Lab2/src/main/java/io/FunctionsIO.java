7192fed4aeed359dd35bc39f7f5ce10f7947a94f
Maslov <sanek@google.com>
2025-10-28 02:10:14 +0400

    Изменение FunctionsIO: добавлен статический метод void writeTabulatedFunction (BufferedWriter writer, TabulatedFunction function),задача которого – записать представление функции function в буферизованный символьный поток writer

--- Lab2/src/main/java/io/FunctionsIO.java
+++ Lab2/src/main/java/io/FunctionsIO.java
@@ -1,7 +1,24 @@
 package io;
 
+import functions.Point;
+import functions.TabulatedFunction;
+import java.io.BufferedWriter;
+import java.io.PrintWriter;
+
 public final class FunctionsIO {
     private FunctionsIO() {
         throw new UnsupportedOperationException();
+    }
+
+    static void writeTabulatedFunction (BufferedWriter writer, TabulatedFunction function) {
+        PrintWriter pw = new PrintWriter(writer);
+
+        pw.println(function.getCount());
+
+        for (Point point : function) {
+            pw.printf("%f %f%n", point.x, point.y);
+        }
+
+        pw.flush();
     }
 }
