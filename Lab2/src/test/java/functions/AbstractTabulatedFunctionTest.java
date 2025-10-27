1d2f92ba5ca05e4a2b4391f9adea6d99edf09958
Maslov <sanek@google.com>
2025-10-27 17:05:06 +0400

    Добавлены тесты для методов checkLengthIsTheSame, checkSorted класса AbstractTabulatedFunction

---
+++ Lab2/src/test/java/functions/AbstractTabulatedFunctionTest.java
@@ -0,0 +1,33 @@
+package functions;
+
+import exceptions.ArrayIsNotSortedException;
+import exceptions.DifferentLengthOfArraysException;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class AbstractTabulatedFunctionTest {
+    @Test
+    void checkLengthIsTheSameTest() {
+        double[] array1 = {1, 89, 100, 152};
+        double[] array2 = {45, 78, 56};
+        assertThrows(DifferentLengthOfArraysException.class,
+                () -> AbstractTabulatedFunction.checkLengthIsTheSame(array1, array2));
+
+        double[] array3 = {1, 89, 100, 152};
+        double[] array4 = {45, 78, 123, 487};
+        assertDoesNotThrow(() -> AbstractTabulatedFunction.checkLengthIsTheSame(array3, array4));
+    }
+
+    @Test
+    void checkSortedTest() {
+        double[] array1 = {1, 89, 100, 152};
+        assertDoesNotThrow(() -> AbstractTabulatedFunction.checkSorted(array1));
+
+        double[] array2 = {45, 78, 56};
+        assertThrows(ArrayIsNotSortedException.class, () -> AbstractTabulatedFunction.checkSorted(array2));
+
+        double[] array3 = {1, 1, 2};
+        assertThrows(ArrayIsNotSortedException.class, () -> AbstractTabulatedFunction.checkSorted(array3));
+    }
+}
