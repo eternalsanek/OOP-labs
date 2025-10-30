7c8a9d22a3739c1be64e8fa6bbb5a9748f2b9828
Tashlintsev <tashlintsev-d-a@yandex.ru>
2025-10-30 18:02:08 +0400

    Небольшое изменение

--- Lab2/src/main/java/operations/TabulatedDifferentialOperator.java
+++ Lab2/src/main/java/operations/TabulatedDifferentialOperator.java
@@ -14,19 +14,15 @@ public class TabulatedDifferentialOperator implements DifferentialOperator<Tabul
         int count = points.length;;
         double[] xValues = new double[count];
         double[] yValues = new double[count];
-        for(int i=0; i < count; i++){
+        for(int i=0; i < count - 1; i++) {
             xValues[i] = points[i].x;
-            if(i != count - 1){
-                double dx = (points[i+1].x - points[i].x);
-                if(Math.abs(dx) < 1e-12){
-                    throw new ArithmeticException("Division by zero in derivative calculation");
-                }
-                yValues[i] = (points[i+1].y - points[i].y)/dx;
+            double dx = (points[i + 1].x - points[i].x);
+            if (Math.abs(dx) < 1e-12) {
+                throw new ArithmeticException("Division by zero in derivative calculation");
             }
-            else{
-                yValues[i] = yValues[i-1];
-            }
+            yValues[i] = (points[i + 1].y - points[i].y) / dx;
         }
+        yValues[count - 1] = yValues[count-2];
         return factory.create(xValues, yValues);
     }
     public TabulatedDifferentialOperator(TabulatedFunctionFactory factory){
