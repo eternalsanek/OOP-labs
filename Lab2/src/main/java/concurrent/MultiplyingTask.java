095dbc6288da85fa8d5cbbfbd6f3bd6168d5ce43
Maslov <sanek@google.com>
2025-11-02 19:13:22 +0400

    В пакете concurrent создан класс MultiplyingTask, реализующий интерфейс Runnable

---
+++ Lab2/src/main/java/concurrent/MultiplyingTask.java
@@ -0,0 +1,21 @@
+package concurrent;
+
+import functions.TabulatedFunction;
+
+public class MultiplyingTask implements Runnable {
+    private TabulatedFunction function;
+
+    public MultiplyingTask(TabulatedFunction function) {
+        this.function = function;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < function.getCount(); i++) {
+            function.setY(i, 2 * function.getY(i));
+        }
+
+        Thread t = Thread.currentThread();
+        System.out.println("Flow " + t.getName() + " completed the task");
+    }
+}
