72008e1bde549bea2ea3f2d41aaf8a19a2057e29
Tashlintsev <tashlintsev-d-a@yandex.ru>
2025-12-21 13:44:51 +0400

    Корректировки в работе Лаб7

--- Lab7/src/components/Layout/Navbar.jsx
+++ Lab7/src/components/Layout/Navbar.jsx
@@ -1,9 +1,9 @@
 import React, { useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { AuthContext } from '../../utils/auth';
+import { useAuth } from '../../utils/auth';
 
 const Navbar = () => {
-  const { isAuthenticated, username, logout } = useContext(AuthContext);
+  const { isAuthenticated, username, logout } = useAuth();
   const navigate = useNavigate();
 
   const handleLogout = () => {
